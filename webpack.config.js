0746764d638a56ae0402c8e59253096b69824739
yin.zhao <yin.zhao@mainiway.com>
2018-03-15 16:20:33 +0800

    add uglyJS in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const uglify = require('uglifyjs-webpack-plugin');  // uglifyJs only supports ES
 
 module.exports = {
     /* "__dirname"是node.js中的一个全局变量，它指向当前执行脚本所在的目录 */
-    devtool: 'eval-source-map',    //'null' 能大大压缩我们的打包代码
+    devtool: 'null',    //'null' 能大大压缩我们的打包代码  source-map  eval-source-map
     entry: __dirname + "/src/index.js",//已多次提及的唯一入口文件
     // 如果不设置output，生产环境和开发环境的时候 index.html 里引入的js脚本的路径不一致, 因为server运行以后生成的文件是在服务器上/根目录下，而run dev生成的文件是在本地的磁盘上
     output: {
@@ -53,6 +53,7 @@ module.exports = {
                         loader: "css-loader",
                         options: {
                             modules: true, // 指定启用css modules
+                            minimize: true
                             //localIdentName: '[name]__[local]--[hash:base64:5]' // 指定css类名格式 class="Greeter__root--hHjmq"  如果不使用这句话 在F12中class名正常显示
                         }
                     }, {
@@ -70,6 +71,7 @@ module.exports = {
            template: __dirname + '/src/index.template.html'  //new 一个这个插件的实例，并传入相关的参数
        }),
        new webpack.HotModuleReplacementPlugin(), //热加载插件，
+       new uglify()
     //    new ExtractTextPlugin("index.css")
     ]
 }
